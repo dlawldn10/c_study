ed881b9c5e151017d5d55fe5acf6935d3dc7aa9d
Jiwoo Lim <jiwoo_ubuntu@DESKTOP-N03HMBS.localdomain>
2022-10-21 23:23:42 +0900

    Solve codeup basic 41-69

--- codeup_41_80.c
+++ codeup_41_80.c
@@ -2,161 +2,248 @@
 
 int main(){
 
-    // printf("Hello\nWorld\n");
-    // printf("\'Hello\'");
-    // printf("\"Hello World\"");
-    // printf("\"!@#$%%^&*()\"");
-    // printf("\"C:\\Download\\hello.cpp\"");
-
-    // int a;
-    // scanf("%d", &a);
-    // printf("%d", a);
-
     // char a;
     // scanf("%c", &a);
-    // printf("%c", a);
-
-    // float a;
-    // scanf("%f", &a);
-    // printf("%f", a);
+    // printf("%c", a+1);
 
     // int a, b;
     // scanf("%d %d", &a, &b);
-    // printf("%d %d", a, b);
+    // printf("%d", a/b);
 
     // int a, b;
-    // scanf("%c %c", &a, &b);
-    // printf("%c %c", b, a);
+    // scanf("%d %d", &a, &b);
+    // printf("%d", a%b);
 
-    // float a;
-    // scanf("%f", &a);
-    // printf("%.2f", a);
-
-    // int a;
-    // scanf("%d", &a);
-    // printf("%d %d %d", a, a, a);
-
-    // int a, b;
-    // scanf("%d:%d", &a, &b);
-    // printf("%d:%d", a, b);
-
-    // int year, month, day;
-    // scanf("%d.%d.%d", &year, &month, &day);
-    // // 02d 이처럼 넣을 경우 1월의 경우 01 이렇게 앞에 0이 채워져서 출력됨.
-    // printf("%04d.%02d.%02d", year, month, day);
-
-    // int a, b;
-    // scanf("%d-%d", &a, &b);
-    // printf("%06d%07d", a, b);
-
-    // // 배열을 이용하여 문자열 string표현한다.
-    // char word[51] = "";
-    // // 배열의 포인터는 기본적으로 배열의 맨 앞자리를 가리키고 있으므로, 배열은 &빼고 넣어도 무방하다.
-    // scanf("%s", &word);
-    // printf("%s", word);
-
-    // char a[2001];
-    // // pintf는 공백 기준으로 구분하여 받기때문에, 한 줄을 공백 포함하여 입력받을때는 fgets 를 이용하여 입력받을 수 있다.
-    // // a라는 문자열에 2000자까지 stdin으로 입력받겠다. 
-    // fgets(a, 2000, stdin);
-    // printf("%s", a);
-
-    // int a, b;
-    // scanf("%d.%d", &a, &b);
-    // printf("%d\n%d", a, b);
-
-    // // 배열로 입력이 들어왔을 때 마지막에 null값이 들어감.
-    // // 따라서 배열의 첫번째 부터 null값의 바로 전까지 값들을 읽는 것.
-    // // abc\0
-    // char a[21];
-    // scanf("%s", a);
-    // // C언어는 반복문 돌리는 변수를 바깥쪽에 선언한다.
-    // int i;
-    // for(i=0; i<21 ;i++){
-    //     if(a[i] == '\0') break;
-    //     printf("\'%c\'\n", a[i]);
-    // }
-
-    // int a, b, c, d, e;
-    // scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
-    // printf("[%d0000]\n", a);
-    // printf("[%d000]\n", b);
-    // printf("[%d00]\n", c);
-    // printf("[%d0]\n", d);
-    // printf("[%d]\n", e);
-
-    // int h, m, s;
-    // scanf("%d:%d:%d", &h, &m, &s);
-    // printf("%d", m);
-
-    // int y, m, d;
-    // scanf("%d.%d.%d", &y, &m, &d);
-    // printf("%02d-%02d-%04d", d, m, y);
-
-    // // 음의 부호 쪽은 표현하지 않는 자료형.
-    // // unsigned int 데이터형을 사용하면 0 ~ 4294967295 범위의 정수를 저장할 수 있다.
-    // unsigned int a;
-    // scanf("%u", &a);
-    // printf("%u", a);
-
-    // // double 데이터형을 사용하면 flaot의 2배에 해당하는 범위를 커버할 수 있다.
-    // double a;
-    // scanf("%lf", &a);
-    // printf("%.11lf", a);
-
-
-    // // long long int 데이터형을 사용하면 
-    // // -9,223,372,036,854,775,808 ~ +9,223,372,036,854,775,807 범위의 정수값을 저장시킬 수 있다.
     // long long int a;
     // scanf("%lld", &a);
-    // printf("%lld", a);
+    // printf("%lld", a+1);
 
-    // // 10진수는 %d, 8진수는 %o (octal).
-    // // 10진수: 0 1 2 3 4 5 6 7 8 9 10
-    // // 8진수: 0 1 2 3 4 5 6 7 10 11 12
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // printf("%d\n", a+b);
+    // printf("%d\n", a-b);
+    // printf("%d\n", a*b);
+    // printf("%d\n", a/b);
+    // printf("%d\n", a%b);
+    // printf("%.2f\n", (float)a/(float)b);
+
+
+    // int a, b, c;
+    // scanf("%d %d %d", &a, &b, &c);
+    // int sum = a + b + c;
+    // float avg = (float)sum / 3;
+    // printf("%d\n%.1f", sum, avg);
+
+
+    // // >> n : 왼쪽에 0을 n개 붙인다. n을 2**n으로 나눈 값.
+    // // << n : 오른쪽에 0을 n개 붙인다. n을 2**n배 한다.
     // int a;
     // scanf("%d", &a);
-    // printf("%o", a);
+    // printf("%d", a << 1);
 
-    // // 16진수는 %x
-    // // 0 1 2 3 4 5 6 7 8 9 a b c d e f
-    // int a;
-    // scanf("%d", &a);
-    // printf("%x", a);
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // printf("%d", a << b);
 
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // if (a>b) printf("1");
+    // else printf("0");
 
-    // // %x를 %X와 같이 대문자로 적어주면 출력도 대문자로 할 수 있다.
-    // int a;
-    // scanf("%d", &a);
-    // printf("%X", a);
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // if (a==b) printf("1");
+    // else printf("0");
 
-    // int a;
-    // scanf("%o", &a);
-    // printf("%d", a);
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // if (a<=b) printf("1");
+    // else printf("0");
 
-    // int a;
-    // scanf("%x", &a);
-    // printf("%o", a);
-
-    // // char를 받아서 %d로 출력하게 되면 저절로 아스키 코드 값을 얻을 수 있다.
-    // char a;
-    // scanf("%c", &a);
-    // printf("%d", a);
-
-    // // int를 받아서 %c로 출력하게 되면 저절로 그 정수 = 아스키코드 값에 해당하는 char를 출력해준다.
-    // // 아스키코드의 범위를 넘어가면 알수없는 문자 나옴.
-    // int a;
-    // scanf("%d", &a);
-    // printf("%c", a);
-
-    // long long int a, b;
-    // scanf("%lld %lld", &a, &b);
-    // printf("%lld", a+b);
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // if (a!=b) printf("1");
+    // else printf("0");
 
     // int a;
     // scanf("%d", &a);
-    // printf("%d", -a);
+    // printf("%d", !a);
 
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // if(a == 1 && b == 1){
+    //     printf("1");
+    // }else{
+    //     printf("0");
+    // }
+
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // if(a == 1 || b == 1){
+    //     printf("1");
+    // }else{
+    //     printf("0");
+    // }
+
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // // printf("%d", (a != b));
+    // // 또는 이렇게도 표현 가능.
+    // printf("%d", (!a&&b)||(a&&!b));
+
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // printf("%d", (a == b));
+    // // 또는 이렇게도 표현 가능.
+    // // printf("%d", (a&&b)||(!a&&!b));
+
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // printf("%d", (!a && !b));
+
+    // // 비트 연산자 ~틸드. ex) 100101 을 011010으로 바꾼다.
+    // int a;
+    // scanf("%d", &a);
+    // printf("%d", ~a);
+
+    // // 비트단위 AND 연산
+    // // 0100 와
+    // // 1101 를 하나씩 비교해서
+    // // => 0100 두개 모두 1이면 1로 표현.
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // printf("%d", (a&b));
+
+    // // 비트단위 OR 연산
+    // // 0100 와
+    // // 1101 를 하나씩 비교해서
+    // // => 1101 둘 중 하나가 1이면 1로 표현.
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // printf("%d", (a|b));
+
+
+    // // 비트단위 XOR 연산(caret)
+    // // 0100
+    // // 1101
+    // // => 1001 둘이 서로 같으면 0, 둘이 서로 다르면 1로 표현.
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // printf("%d", (a^b));
+
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    
+    // // if(a>b){
+    // //     printf("a");
+    // // }else{
+    // //     printf("b");
+    // // }
+    // // 위와 같은 코드를 아래와 같이 입력할 수 있다.
+    // printf("%d", (a>b)? a:b);
+
+
+    // // 조건문 사용
+    // int a, b, c;
+    // scanf("%d %d %d", &a, &b, &c);
+    // if(a <= b && a <= c){
+    //     printf("%d", a);
+    // }else if (b <= a && b <= c)
+    // {
+    //     printf("%d", b);
+    // }else if (c <= a && c <= b)
+    // {
+    //     printf("%d", c);
+    // }
+
+    // // 위와 같은 코드를 아래와 같이 입력할 수 있다.
+    // // 배열 사용
+    // #include <limits.h>
+    // int a[3];
+    // scanf("%d %d %d", &a[0], &a[1], &a[2]);
+    // int min = INT_MAX;
+    // if(a[0] < min){
+    //     min = a[0];
+    // }
+    // if(a[1] < min){
+    //     min = a[1];
+    // }
+    // if(a[2] < min){
+    //     min = a[2];
+    // }
+    // printf("%d", min);
+
+    // // 위와 같은 코드를 아래와 같이 입력할 수 있다.
+    // // 삼항 연산자 사용
+    // int a, b, c;
+    // scanf("%d %d %d", &a, &b, &c);
+    // printf("%d", (a<b ? a:b)<c ? (a<b ? a:b):c);
+
+    // int i, a[3];
+    // scanf("%d %d %d", &a[0], &a[1], &a[2]);
+    // for(i=0;i<3;i++){
+    //     if(a[i]%2==0) printf("%d\n", a[i]);
+    // }
+
+    // int i, a[3];
+    // scanf("%d %d %d", &a[0], &a[1], &a[2]);
+    // for(i=0;i<3;i++){
+    //     if(a[i]%2==0) printf("even\n");
+    //     else printf("odd\n");
+    // }
+
+    // int a;
+    // scanf("%d", &a);
+    // if(a > 0){
+    //     printf("plus\n");
+    // }else{
+    //     printf("minus\n");
+    // }
+
+    // if(a%2==0){
+    //     printf("even\n");
+    // }else{
+    //     printf("odd\n");
+    // }
+
+    // int a;
+    // scanf("%d", &a);
+    // if (90<=a){
+    //     printf("A");
+    // } else if (70<=a && a<90)
+    // {
+    //     printf("B");
+    // } else if (40<=a && a<70)
+    // {
+    //     printf("C");
+    // }else if (0<=a && a<40)
+    // {
+    //     printf("D");
+    // }
+
+
+    char a;
+    scanf("%c", &a);
+
+    switch (a)
+    {
+        case 'A':
+            printf("best!!!");
+            break;
+        case 'B':
+            printf("good!!");
+            break;
+        case 'C':
+            printf("run!");
+            break;
+        case 'D':
+            printf("slowly~");
+            break;
+        default:
+            printf("what?");
+            break;
+    }
+    
 
     return 0;
 }
